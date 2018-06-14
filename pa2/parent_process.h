4e3d3376369273994b8eb40ddfdd90c405136cf2
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-06-15 02:55:39 +0300

    fixup! parent_process.c: ref

--- pa2/parent_process.h
+++ pa2/parent_process.h
@@ -1,15 +1,20 @@
-#define odin 1
-#define nil 0
 
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <stdio.h>
 #include <time.h>
 
-void parent_work(InteractionInfo* interaction_info);
+#define odin 1
+#define RES_FAILURE_OP 1
+
+#define nil 0
+#define RES_SUCCESS_OP 0
+
 void get_all_history_messages(AllHistory * all_history, InteractionInfo* interaction_info);
+
+void parent_work(InteractionInfo* interaction_info);
+
 void wait_children();
 
