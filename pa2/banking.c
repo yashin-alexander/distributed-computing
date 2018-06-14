cb7226ef058c2901535c13941fcd89af439914dd
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-06-15 00:12:01 +0300

    banking.c: refactoring

--- pa2/banking.c
+++ pa2/banking.c
@@ -7,13 +7,13 @@
 void transfer(void * parent_data, local_id src, local_id dst, balance_t amount) {
   InteractionInfo *interaction_info = (InteractionInfo*)parent_data;
 
-  TransferOrder to = {
+  TransferOrder an_order = {
     .s_dst = dst,
     .s_src = src,
     .s_amount = amount
   };
 
-  Message m = {
+  Message msg = {
     .s_header = {
       .s_type = TRANSFER,
       .s_magic = MESSAGE_MAGIC,
@@ -22,7 +22,14 @@ void transfer(void * parent_data, local_id src, local_id dst, balance_t amount) 
     }
   };
 
-  memcpy(m.s_payload, &to, sizeof(TransferOrder));
-  send(interaction_info, src, &m);
-  receive(interaction_info, dst, &m);
+  memcpy(msg.s_payload, &an_order, sizeof(TransferOrder));
+  if (send(interaction_info, src, &msg) != 0) {
+    perror("Transfer(): Send failed");
+    return;
+  }
+
+  if (receive(interaction_info, dst, &msg) != 0) {
+    perror("Transfer(): Receive failed");
+    return;
+  }
 }
