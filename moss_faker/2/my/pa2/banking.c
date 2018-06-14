e38160f8ec6d03fb9f33e354e67479bd2034884f
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-06-14 21:34:20 +0300

    additional

--- moss_faker/2/my/pa2/banking.c
+++ moss_faker/2/my/pa2/banking.c
@@ -7,18 +7,22 @@
 void transfer(void * parent_data, local_id src, local_id dst, balance_t amount) {
   InteractionInfo *interaction_info = (InteractionInfo*)parent_data;
 
-  Message m;
-  TransferOrder to;
-  Message m_received;
-  m.s_header.s_type = TRANSFER;
-  to.s_dst = dst;
-  m.s_header.s_magic = MESSAGE_MAGIC;
-  to.s_src = src;
-  m.s_header.s_payload_len = sizeof(TransferOrder);
-  to.s_amount = amount;
-  m.s_header.s_local_time = get_physical_time();
+  TransferOrder to = {
+    .s_dst = dst,
+    .s_src = src,
+    .s_amount = amount
+  };
+
+  Message m = {
+    .s_header = {
+      .s_type = TRANSFER,
+      .s_magic = MESSAGE_MAGIC,
+      .s_payload_len = sizeof(TransferOrder),
+      .s_local_time = get_physical_time()
+    }
+  };
 
   memcpy(m.s_payload, &to, sizeof(TransferOrder));
   send(interaction_info, src, &m);
-  receive(interaction_info, dst, &m_received);
+  receive(interaction_info, dst, &m);
 }
