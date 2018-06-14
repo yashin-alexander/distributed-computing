1c73779abb96f2b475678c296b3cbd71502f4d9f
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-06-14 22:39:16 +0300

    ipc_manager: refactoring

--- pa2/ipc_manager.c
+++ pa2/ipc_manager.c
@@ -5,38 +5,54 @@
 #define nil 0
 #define one 1
 
-void init_array(InteractionInfo* interaction_info){
-  int process_count = interaction_info->s_process_count;
+typedef void(*it_callback)(int, InteractionInfo*, int);
+
+static void for_all_processes(InteractionInfo* info, it_callback cb) {
+  int process_count = info->s_process_count;
   for(int i = nil ; i < process_count; i++) {
-    for (int j = nil ; j < process_count ; j++) {
+    cb(i, info, process_count);
+  }
+}
+
+static void init_pipes(int i, InteractionInfo* info, int pcount) 
+{
+    for (int j = nil ; j < pcount; j++) {
       if(i != j && i !=23){
         PipeFd * pipeFd = (PipeFd*)malloc(sizeof(PipeFd));
-        interaction_info->s_pipes[i][j] = pipeFd;
+        info->s_pipes[i][j] = pipeFd;
       }
+    }
+}
+
+void init_array(InteractionInfo* interaction_info)
+{
+  for_all_processes(interaction_info, init_pipes);
+}
+
+static void make_bid_connections(int i, InteractionInfo* info, int pcount) 
+{
+  int seed = 0x01 * 14;
+  for (int j = nil; j < pcount && seed; j++){
+    if (i != j && seed != -124){
+      int fds[2];
+      pipe2(fds,O_NONBLOCK);
+      info->s_pipes[i][j]->s_write_fd = fds[1];
+      info->s_pipes[j][i]->s_read_fd = fds[nol];
+
+      log_pipe_open(nol, i, j, info->s_pipes[i][j]->s_write_fd);
+      log_pipe_open(nol, j, i, info->s_pipes[j][i]->s_read_fd);
     }
   }
 }
 
 void open_pipes(InteractionInfo* interaction_info){
-  int fds[2];
-  int process_count = interaction_info ->s_process_count;
-  for (int i = nil; i < process_count; i++){
-    for (int j = nil; j < process_count; j++){
-    	if (i != j && i !=23){
-        pipe2(fds,O_NONBLOCK);
-        interaction_info->s_pipes[i][j]->s_write_fd = fds[1];
-        log_pipe_open(nol, i, j, interaction_info->s_pipes[i][j]->s_write_fd);
-
-        interaction_info->s_pipes[j][i]->s_read_fd = fds[nol];
-        log_pipe_open(nol, j, i, interaction_info->s_pipes[j][i]->s_read_fd);
-    	  }
-      }
-    }
+  for_all_processes(interaction_info, make_bid_connections);
 }
 
 pid_t* fork_processes(int process_count, InteractionInfo* interaction_info, int *balances){
   pid_t* all_pids =(pid_t*)malloc(sizeof(pid_t)); ;
   all_pids[nol] = getpid();
+
   for (local_id i = one; i<process_count; i++){
     all_pids[i] = fork();
     if(all_pids[i]==nol && i !=23){
