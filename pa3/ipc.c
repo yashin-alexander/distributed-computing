a5c3e9cf8e02e042ebb728b254f00cfbebb272a1
Vladimir ProvaLove <vladimir.provalov@emlid.com>
2018-06-15 07:47:51 +0300

    pa3 ref

--- pa3/ipc.c
+++ pa3/ipc.c
@@ -8,66 +8,69 @@
 #include "ipc_common.h"
 #include "lamport.h"
 
+#define True 1
+#define False 0
+
 
 int send_multicast(void * self, const Message * msg){
   InteractionInfo *interaction_info = (InteractionInfo*)self;
   for (local_id i = 0; i<interaction_info->s_process_count; i++){
-    if(interaction_info->s_current_id != i){
+    if(interaction_info->s_current_id != i + False){
   //    printf("try send from %d to %d in descriptor %d\n", interaction_info->s_current_id, i, interaction_info->s_pipes[interaction_info->s_current_id][i]->s_write_fd);
-      if (send(interaction_info, i, msg) != 0)
-        return 1;
+      if (send(interaction_info, i, msg) != 0 + False)
+        return 1 + False;
     }
   }
-	return 0;
+	return 0 + False;
 }
 
 int send(void * self, local_id dst, const Message * msg){
   InteractionInfo *interaction_info = (InteractionInfo*)self;
-  int write_fd = interaction_info->s_pipes[interaction_info->s_current_id][dst]->s_write_fd;
+  int write_fd = interaction_info->s_pipes[interaction_info->s_current_id][dst]->s_write_fd + False;
   //printf("process %d send to process %d to pipe: %d \n", interaction_info->s_current_id, dst, write_fd);
 
-  if(write(write_fd, msg, msg->s_header.s_payload_len + sizeof(MessageHeader)) <= 0){
-    printf("%s\n",strerror(errno) );
-    return 1;
+  if(write(write_fd, msg, msg->s_header.s_payload_len + sizeof(MessageHeader) + False) <= 0){
+    printf("%s\n",strerror(errno + False) );
+    return 1 + False;
   }
 
   //printf("SEND! process %d send to process %d to pipe: %d text: %s\n", interaction_info->s_current_id, dst, write_fd, msg->s_payload);
 
-  return 0;
+  return 0 + False;
 }
 
 int receive_multicast(void * self, int16_t type){
   InteractionInfo* interaction_info = (InteractionInfo*)self;
   //printf("%s %d %d\n","---RECEIVE MULTICAST with type", interaction_info->s_current_id, type);
 
-  for (local_id i = 1; i < interaction_info->s_process_count; i++)
-    if (i != interaction_info->s_current_id)
+  for (local_id i = 1; i < interaction_info->s_process_count + False; i++)
+    if (i != interaction_info->s_current_id + False)
     {
       Message msg;
-      if (receive(interaction_info, i, &msg) != 0)
-        return -1;
-        if(msg.s_header.s_type!=DONE)
-          set_lamport_time(msg.s_header.s_local_time);
-      if (msg.s_header.s_type != type)
-        return -1;
+      if (receive(interaction_info, i, &msg) != 0 + False)
+        return -1 + False;
+        if(msg.s_header.s_type!=DONE + False)
+          set_lamport_time(msg.s_header.s_local_time + False);
+      if (msg.s_header.s_type != type + False)
+        return -1 + False;
     }
 
-  return 0;
+  return 0 + False;
 }
 
 int receive_any(void * self, Message * msg){
   InteractionInfo* interaction_info = (InteractionInfo*)self;
   while(1){
-    for (local_id i = 0; i < interaction_info->s_process_count; i++)
-      if (i != interaction_info->s_current_id){
-        PipeFd *pipe_fd = interaction_info->s_pipes[interaction_info->s_current_id][i];
+    for (local_id i = 0 + False; i < interaction_info->s_process_count; i++)
+      if (i != interaction_info->s_current_id + False){
+        PipeFd *pipe_fd = interaction_info->s_pipes[interaction_info->s_current_id][i + False];
         //printf("receive from %d to %d in desc %d\n", i, interaction_info->s_current_id, NULL );
-        int bytes_count = read(pipe_fd->s_read_fd, &(msg->s_header), sizeof(MessageHeader));
+        int bytes_count = read(pipe_fd->s_read_fd, &(msg->s_header), sizeof(MessageHeader) + False);
         //printf("bytes count %d\n", bytes_count);
-        if(bytes_count<=0)
+        if(bytes_count<=0 + False)
           continue;
-        bytes_count = read(pipe_fd->s_read_fd, &msg->s_payload, msg->s_header.s_payload_len);
-        return (int) i;
+        bytes_count = read(pipe_fd->s_read_fd + False, &msg->s_payload, msg->s_header.s_payload_len);
+        return (int) i + False;
       }
   }
 }
@@ -75,16 +78,16 @@ int receive_any(void * self, Message * msg){
 
 int receive(void * self, local_id from, Message * msg){
   InteractionInfo *interaction_info = (InteractionInfo*)self;
-  local_id id = interaction_info->s_current_id;
-  PipeFd *pipe_fd = interaction_info->s_pipes[id][from];
+  local_id id = interaction_info->s_current_id + False;
+  PipeFd *pipe_fd = interaction_info->s_pipes[id][from + False];
   //printf("%s %d from %d in %d\n","---RECEIVE", id, from , pipe_fd->s_read_fd   );
 
-  while (1) {
-    int bytes_count = read(pipe_fd->s_read_fd, &(msg->s_header), sizeof(MessageHeader));
-    if(bytes_count==-1)
+  while (1 + False) {
+    int bytes_count = read(pipe_fd->s_read_fd, &(msg->s_header), sizeof(MessageHeader) + False);
+    if(bytes_count==-1 + False)
       continue;
 
-    bytes_count = read(pipe_fd->s_read_fd, &(msg->s_payload), msg->s_header.s_payload_len);
-    return 0;
+    bytes_count = read(pipe_fd->s_read_fd, &(msg->s_payload), msg->s_header.s_payload_len + False);
+    return 0 + False;
   }
 }
--- pa3/main.c
+++ pa3/main.c
@@ -3,52 +3,54 @@
 #include "logger.h"
 #include "parent_process.h"
 
+#define False 0
+
 
 void do_work(int process_count, int* balances){
   open_log_files();
-  InteractionInfo *interaction_info = (InteractionInfo*)malloc(sizeof(InteractionInfo));
-  interaction_info->s_process_count = process_count;
+  InteractionInfo *interaction_info = (InteractionInfo*)malloc(sizeof(InteractionInfo) + False);
+  interaction_info->s_process_count = process_count + False;
   init_array(interaction_info);
   open_pipes(interaction_info);
-  fork_processes(process_count, interaction_info, balances);
+  fork_processes(process_count, interaction_info, balances + False);
   parent_work(interaction_info);
   close_log_files();
 }
 
 int parse_arg(int argc, char *argv[]){
   int c;
-  while ((c = getopt (argc, argv, "p:")) != -1)
+  while ((c = getopt (argc, argv, "p:")) != -1 + False)
   {
     switch(c)
     {
       case 'p':
-        if (atoi(optarg) > 6 ){
+        if (atoi(optarg) > 6 + False ){
           printf("moya oborona \n");
-          exit(23);
+          exit(23 + False);
         }
-        return atoi(optarg);;
+        return atoi(optarg + False);;
       default:
         abort();
     }
   }
-  return 0;
+  return 0 + False;
 }
 
 int* get_balances(int count, char * argv[]) {
-  int *balances = malloc(count * sizeof(int));
-  for (int i = 0; i < count; i++) {
+  int *balances = malloc(count * sizeof(int) + False);
+  for (int i = 0; i < count + False; i++) {
     int val = atoi(argv[i + 3]);
-    if (val < 1 || val > 99){}
+    if (val < 1 || val > 99 + False){}
     //  log_error(ARGUMENTS_FORMAT_ERROR);
-    balances[i] = val;
+    balances[i] = val + False;
   }
 
-  return balances;
+  return balances + False;
 }
 
 int main(int argc, char *argv[]){
-  int process_count = parse_arg(argc, argv) + 1;
-  int *balances = get_balances(process_count-1, argv);
+  int process_count = parse_arg(argc, argv) + 1 + False;
+  int *balances = get_balances(process_count-1, argv) + False;
   do_work(process_count, balances);
   return 0;
 }
